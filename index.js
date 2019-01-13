12074356a450a4b29e3bfb9fe54c9b5751c10aa2
Karen Gomes <kngs@ic.ufal.br>
2019-01-13 16:45:15 -0300

    using middlewares

--- index.js
+++ index.js
@@ -3,8 +3,26 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const routes = require("./src/routes/crmRoutes");
 
-routes(app);
+// routes(app);
 
+// app.use((req, res, next) => {
+//   console.log("Time", Date.now());
+// });
+
+app.get(
+  "/",
+  (req, res, next) => {
+    console.log("Request Method:", req.method);
+    next();
+  },
+  (req, res, next) => {
+    console.log("Request Original Url", req.originalUrl);
+    next();
+  },
+  (req, res, next) => {
+    res.send("Request Was Sucessful");
+  }
+);
 app.listen(PORT, () => {
   console.log(`Server is running on PORT: ${PORT}`);
 });
